76943c4aa8114a52aaa65d7992312fcb6675cb29
Li Yun <lucassix.lee@gmail.com>
2024-06-16 14:35:04 +0800

    fix.

--- examples/core/asyncio_coroutine_concurrent_before_py311.py
+++ examples/core/asyncio_coroutine_concurrent_before_py311.py
@@ -5,7 +5,6 @@ Before Python 3.11
 
 import asyncio
 import logging
-import sys
 
 logging.basicConfig(
     level=logging.DEBUG, style='{', format='[{threadName} ({thread})] {message}'
@@ -22,14 +21,15 @@ async def concurrent_task(arg: str) -> tuple[str, str]:
     """run coroutines concurrently."""
     logging.debug(f'run concurrent_task: {arg=}')
 
-    if sys.version_info >= (3, 7):
-        t1 = asyncio.create_task(do_task('1', 3.0), name='t1')
-        t2 = asyncio.create_task(do_task('2', 3.5), name='t2')
-    else:
-        # Low-level APIs
-        loop = asyncio.get_running_loop()
-        t1 = loop.create_task(do_task('1', 3.0), name='t1')
-        t2 = loop.create_task(do_task('2', 3.5), name='t2')
+    t1 = asyncio.create_task(do_task('1', 3.0), name='t1')
+    t2 = asyncio.create_task(do_task('2', 3.5), name='t2')
+
+    # Low-level APIs
+    # Before Python 3.7
+    #
+    # loop = asyncio.get_running_loop()
+    # t1 = loop.create_task(do_task('1', 3.0), name='t1')
+    # t2 = loop.create_task(do_task('2', 3.5), name='t2')
 
     # wait until both tasks are completed
     return await t1, await t2
